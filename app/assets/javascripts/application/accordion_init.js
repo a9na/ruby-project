85228f76ed223668406fb8e4a6a84f631c3c882e
a9na <anaiscoding@gmail.com>
2024-12-18 13:43:49 +0100

    Fix header, spaces, faqs, show html

--- app/assets/javascripts/application/accordion_init.js
+++ app/assets/javascripts/application/accordion_init.js
@@ -23,3 +23,35 @@ $(document).ready(function () {
     }
   });
 });
+
+
+$(function () {
+
+  const $head = $('.js-accordion__head');
+  const $body = $('.js-accordion__body');
+
+  // Prva stavka je otvorena pri učitavanju stranice
+  $('.accordion__item').first().addClass('active');
+  $body.first().slideDown();
+
+  $head.on('click', toggleBody);
+
+  function toggleBody() {
+    var $item = $(this).closest('.accordion__item'); // Pronađi roditelja (accordion__item)
+    var $currentBody = $item.find('.js-accordion__body'); // Pronađi odgovarajući sadržaj
+
+    // Ako je kliknuto pitanje već otvoreno, zatvori ga
+    if ($item.hasClass('active')) {
+      $currentBody.stop(true, true).slideUp(); // Zatvori sadržaj
+      $item.removeClass('active'); // Ukloni 'active' klasu
+    } else {
+      // Ako nije otvoreno, zatvori sve ostale i otvori trenutno
+      $('.accordion__item').removeClass('active'); // Ukloni 'active' sa svih
+      $('.js-accordion__body').slideUp(); // Sakrij sve odgovore
+
+      $item.addClass('active'); // Dodaj 'active' trenutnoj stavci
+      $currentBody.stop(true, true).slideDown(); // Prikazivanje odgovarajućeg sadržaja
+    }
+  }
+
+});
