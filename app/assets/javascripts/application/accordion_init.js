64ed8696f38fbd64e1bb602a08cd7f1a4d42e659
a9na <anaiscoding@gmail.com>
2024-12-19 13:40:07 +0100

    Fix news, spaces, styling

--- app/assets/javascripts/application/accordion_init.js
+++ app/assets/javascripts/application/accordion_init.js
@@ -1,56 +1,26 @@
-$(document).ready(function () {
-  const $head = $('.js-accordion__head');
-  const $body = $('.js-accordion__body');
-
-  const $firstItem = $('.accordion__item').eq(0);
-  $firstItem.addClass('open');
-  $firstItem.find('.js-accordion__body').show();
-
-  $accordionHeaders.on('click', function () {
-    const $currentItem = $(this).closest('.accordion__item');
-    const $currentBody = $currentItem.find('.js-accordion__body');
-
-    if ($currentItem.hasClass('open')) {
-      $currentBody.slideUp(300, function () {
-        $currentItem.removeClass('open');
-      });
-    } else {
-      $('.accordion__item.open').removeClass('open');
-      $accordionBodies.slideUp(300);
-
-      $currentItem.addClass('open');
-      $currentBody.slideDown(300);
-    }
-  });
-});
-
-
 $(function () {
 
   const $head = $('.js-accordion__head');
   const $body = $('.js-accordion__body');
 
-  // Prva stavka je otvorena pri učitavanju stranice
   $('.accordion__item').first().addClass('active');
   $body.first().slideDown();
 
   $head.on('click', toggleBody);
 
   function toggleBody() {
-    var $item = $(this).closest('.accordion__item'); // Pronađi roditelja (accordion__item)
-    var $currentBody = $item.find('.js-accordion__body'); // Pronađi odgovarajući sadržaj
+    var $item = $(this).closest('.accordion__item'); 
+    var $currentBody = $item.find('.js-accordion__body'); 
 
-    // Ako je kliknuto pitanje već otvoreno, zatvori ga
     if ($item.hasClass('active')) {
-      $currentBody.stop(true, true).slideUp(); // Zatvori sadržaj
-      $item.removeClass('active'); // Ukloni 'active' klasu
+      $currentBody.stop(true, true).slideUp(); 
+      $item.removeClass('active'); 
     } else {
-      // Ako nije otvoreno, zatvori sve ostale i otvori trenutno
-      $('.accordion__item').removeClass('active'); // Ukloni 'active' sa svih
-      $('.js-accordion__body').slideUp(); // Sakrij sve odgovore
+      $('.accordion__item').removeClass('active'); 
+      $('.js-accordion__body').slideUp(); 
 
-      $item.addClass('active'); // Dodaj 'active' trenutnoj stavci
-      $currentBody.stop(true, true).slideDown(); // Prikazivanje odgovarajućeg sadržaja
+      $item.addClass('active'); 
+      $currentBody.stop(true, true).slideDown();
     }
   }
 
